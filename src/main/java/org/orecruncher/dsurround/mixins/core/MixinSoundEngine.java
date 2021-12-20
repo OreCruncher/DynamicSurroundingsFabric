40428f4eed7b180dfa3381b00e0fb4c9533915bb
Nico Mexis <nico.mexis@kabelmail.de>
2021-12-20 15:14:02 +0100

    Workaround for conditional crash on world unload

--- src/main/java/org/orecruncher/dsurround/mixins/core/MixinSoundEngine.java
+++ src/main/java/org/orecruncher/dsurround/mixins/core/MixinSoundEngine.java
@@ -1,13 +1,23 @@
 package org.orecruncher.dsurround.mixins.core;
 
 import net.minecraft.client.sound.SoundEngine;
+import net.minecraft.client.sound.Source;
+import org.apache.logging.log4j.Logger;
 import org.orecruncher.dsurround.Client;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SoundEngine.class)
 public class MixinSoundEngine {
+
+    @Shadow @Final
+    static Logger LOGGER;
 
     /**
      * Modify the number of streaming sounds that can be handled by the underlying sound engine.  The number of
@@ -20,4 +30,14 @@ public class MixinSoundEngine {
     public int dsurround_initialize(int v) {
         return Client.Config.soundSystem.streamingChannels;
     }
+
+    @Inject(method = "release",
+            at = @At(value = "INVOKE", shift = At.Shift.BEFORE,
+                    target = "Ljava/lang/IllegalStateException;<init>(Ljava/lang/String;)V"),
+            cancellable = true)
+    public void rlsH(Source source, CallbackInfo ci) {
+        LOGGER.info("Source is being released incorrectly: " + source);
+        ci.cancel();
+    }
+
 }
