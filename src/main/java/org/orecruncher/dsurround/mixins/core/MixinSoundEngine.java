60983e8c7218ce7b45d378ec3aafec29495f31b7
Nico Mexis <nico.mexis@kabelmail.de>
2021-12-21 23:59:32 +0100

    Remove workaround Mixin

--- src/main/java/org/orecruncher/dsurround/mixins/core/MixinSoundEngine.java
+++ src/main/java/org/orecruncher/dsurround/mixins/core/MixinSoundEngine.java
@@ -1,23 +1,13 @@
 package org.orecruncher.dsurround.mixins.core;
 
 import net.minecraft.client.sound.SoundEngine;
-import net.minecraft.client.sound.Source;
-import org.apache.logging.log4j.Logger;
 import org.orecruncher.dsurround.Client;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SoundEngine.class)
 public class MixinSoundEngine {
-
-    @Shadow @Final
-    static Logger LOGGER;
 
     /**
      * Modify the number of streaming sounds that can be handled by the underlying sound engine.  The number of
@@ -30,15 +20,5 @@ public class MixinSoundEngine {
     public int dsurround_initialize(int v) {
         return Client.Config.soundSystem.streamingChannels;
     }
-
-// TODO: Delete if really fixed
-//    @Inject(method = "release",
-//            at = @At(value = "INVOKE", shift = At.Shift.BEFORE,
-//                    target = "Ljava/lang/IllegalStateException;<init>(Ljava/lang/String;)V"),
-//            cancellable = true)
-//    public void rlsH(Source source, CallbackInfo ci) {
-//        LOGGER.info("Source is being released incorrectly: " + source);
-//        ci.cancel();
-//    }
 
 }
