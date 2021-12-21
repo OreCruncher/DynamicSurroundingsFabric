b9584c9038fff7b97214541c862bcbace7820acb
Nico Mexis <nico.mexis@kabelmail.de>
2021-12-21 23:58:07 +0100

    Fix two deprecations

--- src/main/java/org/orecruncher/dsurround/lib/biome/BiomeUtils.java
+++ src/main/java/org/orecruncher/dsurround/lib/biome/BiomeUtils.java
@@ -2,16 +2,16 @@ package org.orecruncher.dsurround.lib.biome;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeKeys;
-import net.minecraft.world.biome.BuiltinBiomes;
 
 public final class BiomeUtils {
 
     // Default to the VOID because PLAINS has data associated with it
     public static final Identifier DEFAULT_ID = BiomeKeys.THE_VOID.getValue();
-    public static final Biome DEFAULT_BIOME = BuiltinBiomes.THE_VOID;
+    public static final Biome DEFAULT_BIOME = BuiltinRegistries.BIOME.get(BiomeKeys.THE_VOID);
 
     public static Biome getPlayerBiome(PlayerEntity player) {
         World world = player.getEntityWorld();
