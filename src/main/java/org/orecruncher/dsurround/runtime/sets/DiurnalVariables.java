2663bd94e8d875f4a4bc775862f8707ceef041e6
OreCruncher <orecruncher@gmail.com>
2024-01-14 14:59:59 -0800

    Config tweaks for bop; fixup biome debug display

--- src/main/java/org/orecruncher/dsurround/runtime/sets/DiurnalVariables.java
+++ src/main/java/org/orecruncher/dsurround/runtime/sets/DiurnalVariables.java
@@ -33,7 +33,7 @@ public class DiurnalVariables extends VariableSet<IDiurnalVariables> implements 
             this.isSunrise = cycle == DayCycle.SUNRISE;
             this.isSunset = cycle == DayCycle.SUNSET;
             this.moonPhaseFactor = DayCycle.getMoonSize(world);
-            this.celestialAngle = world.getSunAngle(0F);
+            this.celestialAngle = world.getTimeOfDay(0F);
         } else {
             this.isDay = false;
             this.isNight = false;
