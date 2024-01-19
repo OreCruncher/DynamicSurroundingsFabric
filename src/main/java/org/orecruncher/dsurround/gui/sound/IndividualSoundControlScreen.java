ef56277a0cb3191a351935f2e5e40ceeb2b8a727
OreCruncher <orecruncher@gmail.com>
2024-01-18 19:51:06 -0800

    Fix sound config GUI; correct loader in fabric json

--- src/main/java/org/orecruncher/dsurround/gui/sound/IndividualSoundControlScreen.java
+++ src/main/java/org/orecruncher/dsurround/gui/sound/IndividualSoundControlScreen.java
@@ -80,7 +80,6 @@ public class IndividualSoundControlScreen extends Screen {
                 GameUtils.getMC(),
                 this.width,
                 this.height,
-                bottomY,
                 topY,
                 bottomY,
                 SELECTION_WIDTH,
