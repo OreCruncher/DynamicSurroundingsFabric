46dd081a22cbec38f92ce348e58294edcf6543a3
OreCruncher <orecruncher@gmail.com>
2024-01-24 19:32:46 -0800

    Add season info to clock display

--- src/main/java/org/orecruncher/dsurround/gui/overlay/ClockOverlay.java
+++ src/main/java/org/orecruncher/dsurround/gui/overlay/ClockOverlay.java
@@ -10,8 +10,10 @@ import org.orecruncher.dsurround.Configuration;
 import org.orecruncher.dsurround.config.libraries.ITagLibrary;
 import org.orecruncher.dsurround.lib.GameUtils;
 import org.orecruncher.dsurround.lib.MinecraftClock;
+import org.orecruncher.dsurround.lib.collections.ObjectArray;
 import org.orecruncher.dsurround.lib.gui.ColorGradient;
 import org.orecruncher.dsurround.lib.gui.ColorPalette;
+import org.orecruncher.dsurround.lib.seasons.ISeasonalInformation;
 import org.orecruncher.dsurround.tags.ItemEffectTags;
 
 public class ClockOverlay extends AbstractOverlay {
@@ -23,16 +25,19 @@ public class ClockOverlay extends AbstractOverlay {
     private static final int BOTTOM_OFFSET = 68 + 20;
 
     private final ITagLibrary tagLibrary;
+    private final ISeasonalInformation seasonalInformation;
     private final Configuration config;
     private final MinecraftClock clock;
     private final ColorGradient gradient;
     private boolean showClock;
-    private int textWidth;
-    private String clockText;
+    private int renderWidth;
+    private int renderHeight;
+    private ObjectArray<String> clockDisplay = new ObjectArray<>(2);
     private int color;
 
-    public ClockOverlay(Configuration config, ITagLibrary tagLibrary) {
+    public ClockOverlay(Configuration config, ITagLibrary tagLibrary, ISeasonalInformation seasonalInformation) {
         this.tagLibrary = tagLibrary;
+        this.seasonalInformation = seasonalInformation;
         this.config = config;
         this.clock = new MinecraftClock();
         this.gradient = new ColorGradient(ColorPalette.DARK_VIOLET, ColorPalette.SUN_GLOW, 180F);
@@ -49,8 +54,17 @@ public class ClockOverlay extends AbstractOverlay {
 
             this.showClock = this.doShowClock(mainHandItem) || this.doShowClock(offHandItem) || this.doShowClock(GameUtils.getMC().crosshairPickEntity);
             this.clock.update(player.level());
-            this.clockText = this.clock.getFormattedTime();
-            this.textWidth = GameUtils.getTextRenderer().width(clockText);
+
+            this.clockDisplay.clear();
+            this.clockDisplay.add(this.clock.getFormattedTime());
+            this.seasonalInformation.getCurrentSeasonTranslated(player.level()).ifPresent(s -> this.clockDisplay.add(s));
+
+            var textRender = GameUtils.getTextRenderer();
+            this.renderWidth = textRender.width(this.clockDisplay.get(0));
+            if (this.clockDisplay.size() > 1)
+                this.renderWidth = Math.max(this.renderWidth, textRender.width(this.clockDisplay.get(1)));
+
+            this.renderHeight = this.clockDisplay.size() == 1 ? textRender.lineHeight - 2 : textRender.lineHeight * 2;
 
             // Calculate the color this tick
             var world = player.level();
@@ -85,12 +99,17 @@ public class ClockOverlay extends AbstractOverlay {
             return;
 
         var textRender = GameUtils.getTextRenderer();
-        var x = (context.guiWidth() - this.textWidth) / 2;
+        var x = context.guiWidth() / 2;
         var y = context.guiHeight() - BOTTOM_OFFSET;
+
+        if (this.clockDisplay.size() == 2)
+            y -= textRender.lineHeight / 2;
 
         // Don't use renderTooltip. It uses a Z which pushes the rendering to the top of the Z stack and can
         // and can interfere with renders.
-        TooltipRenderUtil.renderTooltipBackground(context, x, y, this.textWidth, textRender.lineHeight - 2, 0);
-        context.drawString(textRender, this.clockText, x, y, this.color, false);
+        TooltipRenderUtil.renderTooltipBackground(context, x - this.renderWidth / 2, y, this.renderWidth, this.renderHeight, 0);
+        context.drawCenteredString(textRender, this.clockDisplay.get(0), x, y, this.color);
+        if (this.clockDisplay.size() == 2)
+            context.drawCenteredString(textRender, this.clockDisplay.get(1), x, y + textRender.lineHeight, this.color);
     }
 }
