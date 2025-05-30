b67b0889eea8dbcf9afc791a2f23078c819e705f
OreCruncher <orecruncher@gmail.com>
2025-05-30 07:53:35 -0700

    Clean up item tags

--- common/src/main/java/org/orecruncher/dsurround/tags/ItemEffectTags.java
+++ common/src/main/java/org/orecruncher/dsurround/tags/ItemEffectTags.java
@@ -13,19 +13,19 @@ public class ItemEffectTags {
 
     static final Collection<TagKey<Item>> TAGS = new HashSet<>();
 
-    public static final TagKey<Item> AXES = of("axes");
-    public static final TagKey<Item> BOOKS = of("books");
-    public static final TagKey<Item> BOWS = of("bows");
-    public static final TagKey<Item> CROSSBOWS = of("crossbows");
-    public static final TagKey<Item> MACES = of("maces");
-    public static final TagKey<Item> POTIONS = of("potions");
-    public static final TagKey<Item> SHIELDS = of("shields");
-    public static final TagKey<Item> SPEARS = of("spears");
-    public static final TagKey<Item> SWORDS = of("swords");
-    public static final TagKey<Item> TOOLS = of("tools");
-    public static final TagKey<Item> COMPASSES = of("compasses");
+    public static final TagKey<Item> AXES = of("is_axe");
+    public static final TagKey<Item> BOOKS = of("is_book");
+    public static final TagKey<Item> BOWS = of("is_bow");
+    public static final TagKey<Item> CROSSBOWS = of("is_crossbow");
+    public static final TagKey<Item> MACES = of("is_mace");
+    public static final TagKey<Item> POTIONS = of("is_potion");
+    public static final TagKey<Item> SHIELDS = of("is_shield");
+    public static final TagKey<Item> SPEARS = of("is_spear");
+    public static final TagKey<Item> SWORDS = of("is_sword");
+    public static final TagKey<Item> TOOLS = of("is_tool");
+    public static final TagKey<Item> COMPASSES = of("is_compass");
     public static final TagKey<Item> COMPASS_WOBBLE = of("compass_wobble");
-    public static final TagKey<Item> CLOCKS = of("clocks");
+    public static final TagKey<Item> CLOCKS = of("is_clock");
 
     private static TagKey<Item> of(String id) {
         var tagKey = TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "effects/" + id));
